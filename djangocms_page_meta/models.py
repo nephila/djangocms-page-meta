495cb7481970c41057cec82435a312547ac37969
Sergei Maertens <sergei@maykinmedia.nl>
2014-06-26 16:28:04 +0200

    Adapted to work with custom user model

--- djangocms_page_meta/models.py
+++ djangocms_page_meta/models.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
+from django.core.cache import cache
+from django.db import models
+from django.db.models.signals import post_save, pre_delete
+from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
+
 from cms.extensions import PageExtension, TitleExtension
 from cms.extensions.extension_pool import extension_pool
 from cms.models import Title, Page
-from django.contrib.auth.models import User
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
 from filer.fields.file import FilerFileField
-from django.core.cache import cache
-from django.db.models.signals import post_save, pre_delete
-from django.dispatch import receiver
 
 
 from .utils import get_cache_key
@@ -44,7 +45,7 @@ class PageMeta(PageExtension):
     og_type = models.CharField(_(u'Resource type'), max_length=255,
                                choices=OG_TYPE_CHOICES,
                                help_text=_(u'Use Article for generic pages.'))
-    og_author = models.ForeignKey(User, verbose_name=_(u'Author account'),
+    og_author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_(u'Author account'),
                                   null=True, blank=True)
     og_author_url = models.CharField(_(u'Author Facebook URL'),
                                      max_length=255, default='', blank=True)
