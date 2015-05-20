7b8c8c0e9c4c726d0de5faeb63776bd22d21e3f6
Ryan Stalbow <ryan.stalbow@pancentric.con>
2015-05-20 17:10:33 +0100

    changes migrations to work with django 1.7. Fixed issue with template and import

--- djangocms_page_meta/models.py
+++ djangocms_page_meta/models.py
@@ -2,8 +2,8 @@
 from cms.extensions import PageExtension, TitleExtension
 from cms.extensions.extension_pool import extension_pool
 from cms.models import Title, Page
-from cms.utils.compat.dj import user_model_label
 from django.core.cache import cache
+from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
@@ -44,7 +44,7 @@ class PageMeta(PageExtension):
     og_type = models.CharField(_(u'Resource type'), max_length=255,
                                choices=OG_TYPE_CHOICES,
                                help_text=_(u'Use Article for generic pages.'))
-    og_author = models.ForeignKey(user_model_label, verbose_name=_(u'Author account'),
+    og_author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_(u'Author account'),
                                   null=True, blank=True)
     og_author_url = models.CharField(_(u'Author Facebook URL'),
                                      max_length=255, default='', blank=True)
