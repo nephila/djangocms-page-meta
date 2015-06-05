861badcad6c9df45aee67d06a602a96b29fcc905
Iacopo Spalletti <i.spalletti@nephila.it>
2015-06-05 19:34:48 +0200

    Add support for Django 1.7

---
+++ djangocms_page_meta/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class PageMetaConfig(AppConfig):
+    name = 'djangocms_page_meta'
+    verbose_name = _("django CMS Page Meta")
