3ed21604120779235b63bee9ff4de870fe51c403
Iacopo Spalletti <i.spalletti@nephila.it>
2016-02-25 15:43:34 +0100

    Bump develop version

--- djangocms_page_meta/__init__.py
+++ djangocms_page_meta/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-__version__ = '0.5.9'
+__version__ = '0.5.9.post1'
 __author__ = 'Iacopo Spalletti <i.spalletti@nephila.it>'
 
 default_app_config = 'djangocms_page_meta.apps.PageMetaConfig'
