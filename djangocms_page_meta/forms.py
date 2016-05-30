8cbc5279aec8775c6c10dd86faf7efa51967c11d
Iacopo Spalletti <i.spalletti@nephila.it>
2016-05-30 22:55:36 +0200

    Improved admin for generic attributes

--- djangocms_page_meta/forms.py
+++ djangocms_page_meta/forms.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from django import forms
 
-from .models import TitleMeta
+from .models import GenericMetaAttribute, TitleMeta
 
 
 class TitleMetaAdminForm(forms.ModelForm):
@@ -14,4 +14,17 @@ class TitleMetaAdminForm(forms.ModelForm):
 
     class Meta:
         model = TitleMeta
+        exclude = ()
+
+
+class GenericAttributeInlineForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(GenericAttributeInlineForm, self).__init__(*args, **kwargs)
+        self.fields['attribute'].widget.attrs[
+            'placeholder'
+        ] = GenericMetaAttribute.DEFAULT_ATTRIBUTE
+
+    class Meta:
+        model = GenericMetaAttribute
         exclude = ()
