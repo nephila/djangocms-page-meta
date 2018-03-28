725bc516b1d6fa7990b3d90e3a19cc8298955fac
Iacopo Spalletti <i.spalletti@nephila.it>
2018-03-28 06:10:25 +0200

    Remove description when creating a CMS page

--- tests/test_adminpage.py
+++ tests/test_adminpage.py
@@ -19,10 +19,7 @@ class AdminPageTest(BaseTest):
         """
         request = self.get_page_request(None, self.user, '/', edit=True)
         form = page_admin.get_form(request)
-        self.assertIsInstance(
-            form.base_fields.get('meta_description'),
-            fields.CharField
-        )
+        self.assertEqual(form.base_fields.get('meta_description'), None)
 
     def test_get_form_with_obj(self):
         """
@@ -33,3 +30,16 @@ class AdminPageTest(BaseTest):
         request = self.get_page_request(page1, self.user, '/', edit=True)
         form = page_admin.get_form(request, page1)
         self.assertEqual(form.base_fields.get('meta_description'), None)
+
+    def test_get_form_with_obj_description(self):
+        """
+        Test that the returned form has been modified by the meta patch
+        """
+        page1, _page2 = self.get_pages()
+        title = page1.get_title_obj('en')
+        title.meta_description = 'something'
+        title.save()
+
+        request = self.get_page_request(page1, self.user, '/', edit=True)
+        form = page_admin.get_form(request, page1)
+        self.assertNotEqual(form.base_fields.get('meta_description'), None)
