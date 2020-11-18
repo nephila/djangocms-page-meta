c686377d984a266b6b9bd07ad9c077e7f026fe39
Iacopo Spalletti <i.spalletti@nephila.it>
2020-11-18 12:13:04 +0100

    Release 1.0.0

--- changes/135.feature
+++
@@ -1,1 +0,0 @@
-Update tooling and drop Python 2 / Django < 2.2 compatibility
--- djangocms_page_meta/__init__.py
+++ djangocms_page_meta/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.10.1.dev1"
+__version__ = "1.0.0"
 __author__ = "Iacopo Spalletti <i.spalletti@nephila.it>"
 
 default_app_config = "djangocms_page_meta.apps.PageMetaConfig"
