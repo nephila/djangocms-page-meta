593e574e43ed9deeb463680cebde1ed68da7fda7
Bernardvdv <Bvdv9075@gmail.com>
2021-07-06 15:35:31 +0100

    Update djangocms_page_meta/migrations/0011_auto_20190218_1010.py
    Co-authored-by: Aiky30 <Aiky30@users.noreply.github.com>

--- djangocms_page_meta/migrations/0011_auto_20190218_1010.py
+++ djangocms_page_meta/migrations/0011_auto_20190218_1010.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.db import DatabaseError, migrations, models
 from django.db.migrations.recorder import MigrationRecorder
 
+
 # django cms 3 backwards compatibility for the Title model.
 page_content_model = 'cms.Title'
 try:
