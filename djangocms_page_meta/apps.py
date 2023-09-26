1014939126e54427dae1b3cca7bbe7dc344a0173
Leonardo Cavallucci <leo.cavallucci@gmail.com>
2023-09-26 14:57:59 +0200

    Set default_auto_field in AppConfig to avoid migrations problem if DEFAULT_AUTO_FIELD is different

--- djangocms_page_meta/apps.py
+++ djangocms_page_meta/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class PageMetaConfig(AppConfig):
     name = "djangocms_page_meta"
     verbose_name = _("django CMS Page Meta")
+    default_auto_field = "django.db.models.BigAutoField"
